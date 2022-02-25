4aeb6d1483e98ef1c3c1149a1788325baba3ec54
Davisralves <davialves324@gmail.com>
2022-02-25 19:16:17 -0300

    corrigindo erros de ESlint

--- src/controllers/validateUsername.ts
+++ src/controllers/validateUsername.ts
@@ -1,24 +1,24 @@
-import { NextFunction, Request, Response } from "express";
-import StatusCode from "../enums/statusCode";
+import { NextFunction, Request, Response } from 'express';
+import StatusCode from '../enums/statusCode';
 
 const validateUsername = (req: Request, res: Response, next: NextFunction) => {
-	const { username } = req.body;
-	if (username === undefined) {
-		return res
-			.status(StatusCode.BAD_REQUEST)
-			.json({ error: "Username is required" });
-	}
-	if (typeof username !== "string") {
-		return res
-			.status(StatusCode.UNPROCESSABLE_ENTITY)
-			.json({ error: "Username must be a string" });
-	}
-	if (username.length <= 2) {
-		return res
-			.status(StatusCode.UNPROCESSABLE_ENTITY)
-			.json({ error: "Username must be longer than 2 characters" });
-	}
-	return next();
+  const { username } = req.body;
+  if (username === undefined) {
+    return res
+      .status(StatusCode.BAD_REQUEST)
+      .json({ error: 'Username is required' });
+  }
+  if (typeof username !== 'string') {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY)
+      .json({ error: 'Username must be a string' });
+  }
+  if (username.length <= 2) {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY)
+      .json({ error: 'Username must be longer than 2 characters' });
+  }
+  return next();
 };
 
 export default validateUsername;
