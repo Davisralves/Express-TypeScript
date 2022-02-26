014660ca28eb6e4043dc3e3fb7b1333781c7026b
Davisralves <davialves324@gmail.com>
2022-02-26 16:05:15 -0300

    corrigindo função utilizada

--- src/controllers/registerOrder.ts
+++ src/controllers/registerOrder.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import 'dotenv/config';
 import StatusCode from '../enums/statusCode';
+import OrderService from '../services/Orders';
 
 const registerOrder = async (req: Request, res: Response) => {
   const { products } = req.body;
   const { authorization } = req.headers;
   const { payload: { id: userId } } = jwt.decode(authorization as string) as JwtPayload;
-  const orderId = await registerOrder(userId);
+  const orderId = await OrderService.registerOrder(userId);
   const order = { userId, products };
   console.log(orderId);
   return res.status(StatusCode.CREATED).json({ order });
