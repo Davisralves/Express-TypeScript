1ed84ec9f286b2e8af08368db932c7d1288d99f4
Davisralves <davialves324@gmail.com>
2022-02-26 16:04:37 -0300

    finalizando logicar de registrar nova ordem

--- src/controllers/registerOrder.ts
+++ src/controllers/registerOrder.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import StatusCode from '../enums/statusCode';
-import ProductService from '../services/Products';
 import 'dotenv/config';
+import StatusCode from '../enums/statusCode';
 
 const registerOrder = async (req: Request, res: Response) => {
   const { products } = req.body;
   const { authorization } = req.headers;
   const { payload: { id: userId } } = jwt.decode(authorization as string) as JwtPayload;
   const orderId = await registerOrder(userId);
-  
+  const order = { userId, products };
+  console.log(orderId);
+  return res.status(StatusCode.CREATED).json({ order });
 };
 
 export default registerOrder;
